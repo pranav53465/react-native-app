b142fc328d6363b8de55cd60484b9cbbd331c543
pranav53465 <pranav53465@gmail.com>
2022-04-09 18:00:17 +0800

    Initial Commit

---
+++ components/index.js
@@ -0,0 +1,17 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from "./Button";
+import DetailsDescription from "./DetailsDescription";
+import DetailsBid from "./DetailsBid";
+import { SubInfo } from "./SubInfo";
+export {
+  HomeHeader,
+  NFTCard,
+  FocusedStatusBar,
+  CircleButton,
+  RectButton,
+  DetailsBid,
+  DetailsDescription,
+  SubInfo,
+};
